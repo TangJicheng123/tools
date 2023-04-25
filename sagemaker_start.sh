fd6fc79838f5db71df6fa7c6d85895e6d519245a
TangJicheng <tang.j@ctw.inc>
2023-04-25 11:33:04 +0900

    add sagemaker_start.sh

---
+++ sagemaker_start.sh
@@ -0,0 +1,9 @@
+source ~/.bashrc
+cd /home/ec2-user/SageMaker
+conda create -n py310 python=3.10
+conda activate py310
+git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
+cd stable-diffusion-webui/models/Stable-diffusion
+aws s3 cp s3://staging-g123-ai/sagemaker/model/diffusion_model/pretrained_model/Old_SD/bg_generation_model/ . --recursive
+cd /home/ec2-user/SageMaker/stable-diffusion-webui
+./webui.sh --share
