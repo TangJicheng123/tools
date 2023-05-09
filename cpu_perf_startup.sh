e09800bacaae82d0c3e7b43f2c67cb29812a68f5
TangJicheng <tang.j@ctw.inc>
2023-05-09 10:24:56 +0900

    update

--- cpu_perf_startup.sh
+++ cpu_perf_startup.sh
@@ -1,5 +1,6 @@
-sudo yum install git gcc g++ htop mesa-libGL.x86_64
+sudo yum install -y git gcc g++ htop mesa-libGL.x86_64
 git clone https://github.com/TangJicheng123/cpu_test.git
+git clone https://gitlab.com/juliensimon/huggingface-demos.git
 wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-2022.05-Linux-x86_64.sh
 
 chmod a+x ./Anaconda3-2022.05-Linux-x86_64.sh
@@ -11,3 +12,18 @@ conda activate cpu_test
 
 cd cpu_test
 pip install -r requirements.txt
+
+cd vino
+python vino2.py
+
+mo --input_model text_encoder.onnx --compress_to_fp16
+mo --input_model vae_decoder.onnx --compress_to_fp16
+mo --input_model vae_encoder.onnx --compress_to_fp16
+mo --input_model unet/unet.onnx --compress_to_fp16
+
+mo --input_model text_encoder.onnx --data_type half
+mo --input_model vae_decoder.onnx --data_type half
+mo --input_model vae_encoder.onnx --data_type half
+mo --input_model unet/unet.onnx --data_type half
+
+python vino3.py
