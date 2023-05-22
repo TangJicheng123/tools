df50e9d895c6d0c2a33b3e841558d9dda746846c
TangJicheng123 <130357271+TangJicheng123@users.noreply.github.com>
2023-05-22 09:54:09 +0900

    add base64 test

---
+++ tools/base64_test.py
@@ -0,0 +1,20 @@
+import base64
+
+def encode_base64(data):
+    encoded_bytes = base64.b64encode(data)
+    encoded_string = encoded_bytes.decode('utf-8')
+    return encoded_string
+
+def decode_base64(encoded_string):
+    decoded_bytes = base64.b64decode(encoded_string)
+    decoded_data = decoded_bytes.decode('utf-8')
+    return decoded_data
+
+# Base64编码示例
+data = b'Hello, World!'  # 要编码的数据（字节字符串）
+encoded_string = encode_base64(data)
+print("Base64编码结果:", encoded_string)
+
+# Base64解码示例
+decoded_data = decode_base64(encoded_string)
+print("Base64解码结果:", decoded_data)
