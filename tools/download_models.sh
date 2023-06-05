baae1ba36c50922bd8ee4b6a0a2a6bbb8d976f77
TangJicheng123 <130357271+TangJicheng123@users.noreply.github.com>
2023-06-05 17:00:00 +0900

    update download

--- tools/download_models.sh
+++ tools/download_models.sh
@@ -1,3 +1,5 @@
+wget https://civitai.com/api/download/models/30163 -O AnythingV5.safetensors
+wget https://civitai.com/api/download/models/72396 -O Lyriel.safetensors
 wget https://civitai.com/api/download/models/76907 -O GhostMix.safetensors
 wget https://civitai.com/api/download/models/79068 -O majicMIX_realistic.safetensors
 wget https://civitai.com/api/download/models/57618 -O Counterfeit-V3.0.safetensors
@@ -10,14 +12,13 @@ wget https://civitai.com/api/download/models/64094 -O NeverEndingDream.safetenso
 wget https://civitai.com/api/download/models/77903 -O XXMix_9realistic.safetensors
 wget https://civitai.com/api/download/models/70969 -O BreakDomain.safetensors
 wget https://civitai.com/api/download/models/64046 -O MeinaHentai.safetensors
-wget https://civitai.com/api/download/models/30163 -O AnythingV5.safetensors
 wget https://civitai.com/api/download/models/15236 -O Deliberate.safetensors
 wget https://civitai.com/api/download/models/46846 -O ReVAnimated.safetensors
-wget https://civitai.com/api/download/models/72396 -O Lyriel.safetensors
 wget https://civitai.com/api/download/models/17233 -O AbyssOrangeMix3.safetensors
 wget https://civitai.com/api/download/models/15640 -O UberRealisticPornMerge.safetensors
 wget https://civitai.com/api/download/models/34070 -O Clarity.safetensors
 wget https://civitai.com/api/download/models/56498 -O AnalogMadness.safetensors
+wget https://civitai.com/api/download/models/11745 -O ChilloutMix.safetensors
 
 # Lora
 wget https://civitai.com/api/download/models/69243 -O lora_Ghibli_Background.safetensors
@@ -25,3 +26,6 @@ wget https://civitai.com/api/download/models/45229 -O Concept_Scenery_scene_V4.s
 wget https://civitai.com/api/download/models/19998 -O Howls_Moving_Castle.safetensors
 wget https://civitai.com/api/download/models/75526 -O GHIBLI_Background.safetensors
 wget https://civitai.com/api/download/models/80594 -O paper_cut_background.safetensors
+
+# VAE
+wget https://civitai.com/api/download/models/28569 -O kl-f8-anime2-vae.ckpt
