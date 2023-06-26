e69fef0799c903d0b85cc29b08115bcf08e99146
TangJicheng123 <130357271+TangJicheng123@users.noreply.github.com>
2023-06-27 01:05:53 +0900

    delete transformers

--- transformers_test/simple.py
+++
@@ -1,3 +0,0 @@
-from huggingface_hub import snapshot_download
-snapshot_location = snapshot_download(repo_id="runwayml/stable-diffusion-v1-5", ignore_patterns=["*.safetensors", "*.ckpt"], cache_dir="/Users/ctw/cache_dir")
-snapshot_location
