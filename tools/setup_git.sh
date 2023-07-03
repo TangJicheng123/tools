35edd9e4cadfdf56047d767d034a2ba432107902
tang <tangjch15@gmail.com>
2023-07-03 02:22:08 +0000

    add setup git command

---
+++ tools/setup_git.sh
@@ -0,0 +1,6 @@
+git config --global user.name "tang"
+git config --global user.email "tangjch15@gmail.com"
+git config --global core.editor vim
+git config --global merge.tool meld
+git config --global color.ui true
+git config --global --list
