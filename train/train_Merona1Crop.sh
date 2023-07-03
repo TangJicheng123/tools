7865e1edacd95f9c1ff82e5e9e64b38c0e27feb1
tang <tangjch15@gmail.com>
2023-07-03 09:54:25 +0000

    modified:   train/train_Merona1Crop.sh

--- train/train_Merona1Crop.sh
+++ train/train_Merona1Crop.sh
@@ -8,11 +8,11 @@ accelerate launch --mixed_precision="fp16" train_dreambooth_diffusers.py \
         --output_dir="/home/ec2-user/tools/train/Merona1_model" \
         --instance_prompt="artifex-queensblade-Merona1Crop" \
         --resolution=512 \
-        --train_batch_size=1 \
+        --train_batch_size=4 \
         --use_8bit_adam \
         --gradient_accumulation_steps=4 --gradient_checkpointing \
         --learning_rate=5e-6 \
         --lr_scheduler="cosine"\
         --lr_warmup_steps=0 \
-        --max_train_steps=100 \
+        --num_train_epochs=100 \
         --mixed_precision=fp16
