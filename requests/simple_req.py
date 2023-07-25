3219e106af16d27c3643bb55f11cdd04b0a3c344
tangjicheng <tangjch15@gmail.com>
2023-07-25 06:54:32 +0000

    refactor: update webui request

--- requests/simple_req.py
+++ requests/simple_req.py
@@ -58,7 +58,60 @@ data = {
     "alwayson_scripts": {}
     }
 
-response = requests.post(base_url + txt2img_path, data=json.dumps(data))
+input2 = '''{
+                "enable_hr": false,
+                "denoising_strength": 0.5,
+                "firstphase_width": 0,
+                "firstphase_height": 0,
+                "hr_scale": 2,
+                "hr_upscaler": "string",
+                "hr_second_pass_steps": 0,
+                "hr_resize_x": 0,
+                "hr_resize_y": 0,
+                "hr_sampler_name": "string",
+                "hr_prompt": "",
+                "hr_negative_prompt": "",
+                "prompt": "1girl,  <lora:test_lora:1:0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0>",
+                "styles": [
+                    "string"
+                ],
+                "seed": 123,
+                "subseed": 123,
+                "subseed_strength": 0,
+                "seed_resize_from_h": -1,
+                "seed_resize_from_w": -1,
+                "sampler_name": "LMS",
+                "batch_size": 1,
+                "n_iter": 1,
+                "steps": 20,
+                "cfg_scale": 7,
+                "width": 512,
+                "height": 512,
+                "restore_faces": false,
+                "tiling": false,
+                "do_not_save_samples": false,
+                "do_not_save_grid": false,
+                "negative_prompt": "",
+                "eta": 0,
+                "s_min_uncond": 0,
+                "s_churn": 0,
+                "s_tmax": 0,
+                "s_tmin": 0,
+                "s_noise": 1,
+                "override_settings": {},
+                "override_settings_restore_afterwards": true,
+                "script_args": [],
+                "sampler_index": "Euler",
+                "script_name": "",
+                "send_images": true,
+                "save_images": false,
+                "alwayson_scripts": {}
+                }'''
+
+data2 = json.loads(input2)
+response = requests.post(base_url + txt2img_path, json=data2)
+
+# response = requests.post(base_url + txt2img_path, data=json.dumps(data))
 
 data = response.json()
 
@@ -66,5 +119,5 @@ i = 0
 for image_str in data["images"]:
     i += 1
     image_data = base64.b64decode(image_str)
-    with open(str(i)+".jpg", "wb") as f:
+    with open("api_12.jpg", "wb") as f:
         f.write(image_data)
