c6e13554a8be2f0e78c0e3026fd998838cb0070c
tangjicheng <tangjch15@gmail.com>
2023-07-27 01:15:57 +0900

    chore: test go json

--- go/t5.go
+++ go/t5.go
@@ -1,29 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
-type Animal struct {
-	name   string
-	weight float32
-}
-
-func (a Animal) Eat() {
-	fmt.Println("Animal is eating...")
-}
-
-type Dog struct {
-	Animal // Animal 结构体嵌套到 Dog 结构体中
-	breed  string
-}
-
-func (d Dog) Bark() {
-	fmt.Println("Dog is barking...")
+type Person struct {
+	Name     string `json:"fullName"`      // 指定字段在JSON中的名称为"fullName"
+	Age      int    `json:"age,omitempty"` // 省略空值字段，如果Age为0，则在JSON中省略该字段
+	Email    string `json:"-"`             // 在JSON编码/解码时忽略Email字段
+	Password string `json:"password,omitempty"`
 }
 
 func main() {
-	d := Dog{Animal: Animal{name: "ddd", weight: 3.4}, breed: "hahahaha"}
-	d.Bark()
-	d.Eat()
+	p := Person{
+		Name:  "John Doe",
+		Age:   30,
+		Email: "john.doe@example.com",
+	}
+
+	// 将结构体转换为JSON
+	jsonData, err := json.Marshal(p)
+	if err != nil {
+		fmt.Println("JSON encoding error:", err)
+		return
+	}
+
+	fmt.Println(string(jsonData))
 }
