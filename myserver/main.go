3dc150c0fb18c02b0844d314643bc9d65f845b10
TangJicheng <130357271+TangJicheng123@users.noreply.github.com>
2023-07-26 16:56:37 +0900

    refactor: call function

--- myserver/main.go
+++ myserver/main.go
@@ -9,25 +9,30 @@ import (
 // 通过字典模拟 DB
 var db = make(map[string]string)
 
+func ping(c *gin.Context) {
+	c.String(http.StatusOK, "pong")
+}
+
+func nameValue(c *gin.Context) {
+	user := c.Params.ByName("name")
+	value, ok := db[user]
+	state := "OK"
+	if !ok {
+		state = "ERROR"
+		value = "None"
+	}
+	c.JSON(http.StatusOK, gin.H{"status": state, "user": user, "value": value})
+}
+
 func setupRouter() *gin.Engine {
 	// 初始化 Gin 框架默认实例，该实例包含了路由、中间件以及配置信息
 	r := gin.Default()
 
 	// Ping 测试路由
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
+	r.GET("/ping", ping)
 
 	// 获取用户数据路由
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
-	})
+	r.GET("/user/:name", nameValue)
 
 	// 需要 HTTP 基本授权认证的子路由群组设置
 	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
