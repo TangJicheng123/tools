49208115b1d82498c9c954bf2f0528bf803d15d6
tangjicheng <tangjch15@gmail.com>
2023-07-27 23:02:48 +0900

    feat: test http query

--- myserver/main2.go
+++ myserver/main2.go
@@ -17,6 +17,15 @@ func main2() {
 
 func queryHandler(c *gin.Context) {
 	name := c.Query("name")
+
+	value := c.DefaultQuery("value", "None")
+
+	ext, ok := c.GetQuery("ext")
+	fmt.Println("[my] default ext: <", ext, ">")
+	if !ok {
+		ext = "haha"
+	}
+
 	fmt.Printf("[my] %s\n", name)
-	c.String(http.StatusOK, "Your name: %s", name)
+	c.String(http.StatusOK, "Your name: %s, value: %s, ext: %s", name, value, ext)
 }
