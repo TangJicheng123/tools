0585b325c06b8367be2ee9eddccce4b82e61ae56
tangjicheng <tangjch15@gmail.com>
2023-07-27 22:58:16 +0900

    feat: parse http param

--- myserver/main2.go
+++ myserver/main2.go
@@ -1,22 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
 
-// 控制器函数
-func doLogin(c *gin.Context) {
-	// 获取post请求参数
-	username := c.PostForm("username")
-	password := c.PostForm("password")
-
-	// 通过请求上下文对象Context, 直接往客户端返回一个字符串
-	c.String(200, "username=%s,password=%s", username, password)
-}
+	"github.com/gin-gonic/gin"
+)
 
 func main2() {
 	r := gin.Default()
 
-	// 路由定义post请求, url路径为：/user/login, 绑定doLogin控制器函数
-	r.POST("/user/login", doLogin)
+	r.POST("/api", queryHandler)
 
 	r.Run(":8080")
+}
+
+func queryHandler(c *gin.Context) {
+	name := c.Query("name")
+	fmt.Printf("[my] %s\n", name)
+	c.String(http.StatusOK, "Your name: %s", name)
 }
