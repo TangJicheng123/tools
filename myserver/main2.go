03fab161a53569ac9f7cf491606d7ec5f22ce74d
tangjicheng <tangjch15@gmail.com>
2023-07-27 23:43:38 +0900

    feat: test http param

--- myserver/main2.go
+++ myserver/main2.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +12,7 @@ func main2() {
 	r := gin.Default()
 
 	r.POST("/api", queryHandler)
+	r.POST("/user/:id", userHandler)
 
 	r.Run(":8080")
 }
@@ -28,4 +30,21 @@ func queryHandler(c *gin.Context) {
 
 	fmt.Printf("[my] %s\n", name)
 	c.String(http.StatusOK, "Your name: %s, value: %s, ext: %s", name, value, ext)
+}
+
+type user struct {
+	ID int `json:"id"`
+}
+
+func userHandler(c *gin.Context) {
+	id := c.Param("id")
+	fmt.Println("[my]", id)
+	idInt, err := strconv.Atoi(id)
+	httpCode := http.StatusOK
+	if err != nil {
+		idInt = -1
+		httpCode = http.StatusBadRequest
+	}
+	ret := user{ID: idInt}
+	c.JSON(httpCode, ret)
 }
