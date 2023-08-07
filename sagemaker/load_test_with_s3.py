8e24ae86372a460b7dc63d83c6784f381f06cdaf
TangJicheng <130357271+TangJicheng123@users.noreply.github.com>
2023-08-07 15:28:20 +0900

    fix: update listfile

--- sagemaker/load_test_with_s3.py
+++ sagemaker/load_test_with_s3.py
@@ -1,6 +1,6 @@
 from torchdata.datapipes.iter import IterableWrapper, S3FileLoader
 
-dp_s3_urls = IterableWrapper(['s3://bucket-name/folder/', ...]).list_files_by_s3()
+dp_s3_urls = IterableWrapper(['s3://staging-g123-ai/sagemaker/datasets/anime-image/train_data/canny_250k/princess/']).list_files_by_s3()
 # In order to make sure data are shuffled and sharded in the
 # distributed environment, `shuffle`  and `sharding_filter`
 # are required. For detail, please check our tutorial in:
