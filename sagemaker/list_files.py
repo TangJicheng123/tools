5357ba72f6d31702bffa381fcebda01c2cc8bde9
TangJicheng <130357271+TangJicheng123@users.noreply.github.com>
2023-08-07 14:42:16 +0900

    feat: add load from s3

---
+++ sagemaker/list_files.py
@@ -0,0 +1,16 @@
+from torchdata.datapipes.iter import IterableWrapper, S3FileLister
+
+s3_prefixes = IterableWrapper(['s3://staging-g123-ai/staging-g123-ai/sagemaker/datasets/anime-image/train_data/canny_250k/princess/', ...])
+
+dp_s3_urls = S3FileLister(s3_prefixes)
+for d in dp_s3_urls:
+    print(d)
+    pass
+
+print("****test2*****")
+
+# Functional API
+dp_s3_urls = s3_prefixes.list_files_by_s3(request_timeout_ms=100)
+for d in dp_s3_urls:
+    print(d)
+    pass
