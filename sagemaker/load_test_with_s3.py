78210c60f5a30aa36d028461da95610694e608b5
TangJicheng <130357271+TangJicheng123@users.noreply.github.com>
2023-08-07 16:46:51 +0900

    feat: use tar 2

--- sagemaker/load_test_with_s3.py
+++ sagemaker/load_test_with_s3.py
@@ -1,7 +1,9 @@
+import datetime
+
 from torchdata.datapipes.iter import IterableWrapper, S3FileLoader
 
-dp_s3_urls = IterableWrapper(['s3://staging-g123-ai/users/tangjicheng/pricenss.tar']).list_files_by_s3()
-print(f"dp_s3_urls: {dp_s3_urls}")
+dp_s3_urls = IterableWrapper(['s3://staging-g123-ai/users/tangjicheng/p2.tar']).list_files_by_s3()
+print(f"[{datetime.datetime.now()}] dp_s3_urls: {dp_s3_urls}")
 # In order to make sure data are shuffled and sharded in the
 # distributed environment, `shuffle`  and `sharding_filter`
 # are required. For detail, please check our tutorial in:
@@ -13,7 +15,7 @@ i = 0
 # for url, fd in dp_s3_files: # Start loading data
 #     data = fd.read()
 
-print("Functional API")
+print(f"[{datetime.datetime.now()}] Functional API")
 print(f"sharded_s3_urls.load_files_by_s3: {sharded_s3_urls.load_files_by_s3}")
 # Functional API
 dp_s3_files = sharded_s3_urls.load_files_by_s3(buffer_size=256)
@@ -21,3 +23,5 @@ for url, fd in dp_s3_files:
     data = fd.read()
     i += 1
     print(f"i: {i}")
+
+print(f"[{datetime.datetime.now()}] End")
