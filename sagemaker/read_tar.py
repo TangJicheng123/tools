0d48d18582f0c07d4782b7003d02ade7c3422928
TangJicheng <130357271+TangJicheng123@users.noreply.github.com>
2023-08-07 17:00:11 +0900

    feat: use tar 4

---
+++ sagemaker/read_tar.py
@@ -0,0 +1,29 @@
+import datetime
+import time
+import tarfile
+
+def extract_tar_files(archive_filename):
+    try:
+        with tarfile.open(archive_filename, 'r') as tar:
+            for member in tar.getmembers():
+                # 忽略目录，只处理文件
+                if member.isfile():
+                    file_data = tar.extractfile(member)
+                    if file_data:
+                        content = file_data.read()
+                        print(f"File name: {member.name}")
+                        # print(f"Content:\n{content}\n")
+    except tarfile.TarError as e:
+        print(f"Error while extracting the tar file: {e}")
+
+archive_filename = '/home/ec2-user/SageMaker/s3mount/users/tangjicheng/p2.tar'
+print(f"[{datetime.datetime.now()}] start")
+start_time = time.time()
+
+extract_tar_files(archive_filename)
+
+print(f"[{datetime.datetime.now()}] end")
+end_time = time.time()
+
+cost_time = end_time - start_time
+print(f"cost: {cost_time}")
