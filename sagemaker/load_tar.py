1512c99cff7f7df56539e45199693da6054caadc
TangJicheng <130357271+TangJicheng123@users.noreply.github.com>
2023-08-09 14:54:45 +0900

    feat: add load tar

--- sagemaker/load_tar.py
+++ sagemaker/load_tar.py
@@ -1,2 +1,24 @@
 tar_dir = "/home/ec2-user/SageMaker/pricenss_pack"
-range_tar = "0~46"
+url = "/home/ec2-user/SageMaker/pricenss_pack/{00000000..00000046}.tar"
+
+import time
+import webdataset as wds
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+
+def get_wds_loader():
+    dataset = wds.WebDataset(url)
+    dataloader = DataLoader(dataset, batch_size=1)
+    return dataloader
+
+loader = get_wds_loader()
+
+def run_test(loader):
+    i = 0
+    for item in loader:
+        short = item['short_prompt']
+        print(f"[{i}] {short}")
+        i += 1
+
+if __name__ == "__main__":
+    run_test(loader=loader)
