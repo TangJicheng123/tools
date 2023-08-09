c18d0b07e069cef510efa2939dc31aafa89cbb7a
TangJicheng <130357271+TangJicheng123@users.noreply.github.com>
2023-08-09 15:31:10 +0900

    feat: add load from aws s3

--- sagemaker/load_tar.py
+++ sagemaker/load_tar.py
@@ -1,24 +1,35 @@
-tar_dir = "/home/ec2-user/SageMaker/pricenss_pack"
-url = "/home/ec2-user/SageMaker/pricenss_pack/{00000000..00000046}.tar"
+local_url = "/home/ec2-user/SageMaker/pricenss_pack/{00000000..00000046}.tar"
+
+s3_url = "pipe:aws s3 cp s3://staging-g123-ai/users/tangjicheng/data/{00000000..00000046}.tar"
 
 import time
+import datetime
 import webdataset as wds
 from torch.utils.data import DataLoader
 from tqdm.auto import tqdm
 
-def get_wds_loader():
+def get_wds_loader(url):
     dataset = wds.WebDataset(url)
     dataloader = DataLoader(dataset, batch_size=8, num_workers=4)
     return dataloader
 
-loader = get_wds_loader()
+loader = get_wds_loader(s3_url)
 
 def run_test(loader):
     i = 0
     for item in loader:
         short = item['short_prompt']
-        print(f"[{i}] {len(short)}")
         i += 1
+        if i % 100 == 0:
+            print(f"[{i}] {short}")
 
 if __name__ == "__main__":
+    print(f"[{datetime.datetime.now()}] start test")
+    start_time = time.time()
+
     run_test(loader=loader)
+
+    print(f"[{datetime.datetime.now()}] load end")
+    end_time = time.time()
+
+    print(f"cost: {end_time - start_time} s")
