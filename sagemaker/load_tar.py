cfe8bd20f7b766c7784e2462288b6c45429a3be6
TangJicheng <130357271+TangJicheng123@users.noreply.github.com>
2023-08-10 10:11:57 +0900

    feat: add perf load tar

--- sagemaker/load_tar.py
+++ sagemaker/load_tar.py
@@ -7,10 +7,11 @@ import datetime
 import webdataset as wds
 from torch.utils.data import DataLoader
 from tqdm.auto import tqdm
+from PIL import Image
 
 def get_wds_loader(url):
     dataset = wds.WebDataset(url)
-    dataloader = DataLoader(dataset, batch_size=8, num_workers=4)
+    dataloader = DataLoader(dataset, batch_size=1, num_workers=4)
     return dataloader
 
 loader = get_wds_loader(s3_url)
@@ -18,10 +19,15 @@ loader = get_wds_loader(s3_url)
 def run_test(loader):
     i = 0
     for item in loader:
-        short = item['short_prompt']
+        short_prompt = item['short_prompt']
+        long_prompt = item['long_prompt']
+        img = item['img.jpg']
+        cond_img = item['cond_img.jpg']
+        image_pil = Image.open(img)
+        width, height = image_pil.size
         i += 1
         if i % 100 == 0:
-            print(f"[{i}] {short}")
+            print(f"[{i}] [{datetime.datetime.now()}] long_len:{len(long_prompt)}, {width}, {height}")
 
 if __name__ == "__main__":
     print(f"[{datetime.datetime.now()}] start test")
